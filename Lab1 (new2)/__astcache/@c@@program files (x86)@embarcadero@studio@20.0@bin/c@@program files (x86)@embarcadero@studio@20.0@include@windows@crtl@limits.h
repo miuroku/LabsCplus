db44277c0b44c95dbe11ebe6cd71866f2fa7a322
Maks <thebestguy1101001@gmail.com>
2020-05-15 23:27:51 +0300

    first commit

---
+++ Lab1 (new2)/__astcache/@c@@program files (x86)@embarcadero@studio@20.0@bin/c@@program files (x86)@embarcadero@studio@20.0@include@windows@crtl@limits.h
@@ -0,0 +1,13 @@
+/* Limits.h - stub redirector for _lim.h
+*/
+
+/*
+ *      C/C++ Run Time Library - Version 26.0
+ *
+ *      Copyright (c) 2002, 2019 by Embarcadero Technologies, Inc.
+ *      All Rights Reserved.
+ *
+ */
+
+
+# include <_lim.h>
